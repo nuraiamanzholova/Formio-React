4356abf534db158f9bbcec7d050805e9487ffc92
nuraiamanzholova <amanzholova@publicplan.de>
2023-12-07 16:04:51 +0100

    bootstrap nav/Navbar

--- hello/src/components/ModalF.js
+++ hello/src/components/ModalF.js
@@ -13,16 +13,16 @@ const ModalF = () => {
       />
       {modal && (
         <>
-          <div id="background" className="background"></div>@
+          <div id="background" className="background"></div>
           <div id="modal" className="modal">
             <div className="modal-content">
               <span className="close" id="close" onClick={ModalFensterpopt}>
                 &times;
               </span>
               <h2 className="logotitle">Logo</h2>
-              <div className="logo-container">
-              
-                <img src={logo} alt="Logo" className="logo-container" />
+              <div className="logo-container"> <div/>
+              <div/>
+                
               </div>
             </div>
           </div>
--- hello/src/components/menu.js
+++ hello/src/components/menu.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink, Link } from 'react-router-dom'
 import Startseite from '../pages/Startseite';
 import Stadte from "../pages/Stadte";
 import NotFound from '../pages/Notfound';
@@ -6,39 +6,67 @@ import Uberuns from "../pages/Uberuns"
 import logo from './logo.jpg';
 import ModalF from './ModalF';
 import FormioForm from '../FormioForm';
-import "../Styles/css.css"
-
-const Menu = () => {
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "../Styles/style.css"
+function Menu() {
     return (
-        <>
-            <BrowserRouter>
-                <nav>
-                    <div id="navi">
-                        <div className='header'> <h1> Autowash</h1></div>
-                        <img src={logo} className="logo" alt="logo" />
-                        <div id='navbar'>
-                            <ul>
-                            <li><NavLink to="Formular">Link zum Formular</NavLink></li>
-                                <li> <NavLink to="/"> Startseite</NavLink>  </li>
-                                <li>  <NavLink to="Stadte"> Städte</NavLink></li>
-                                <li> <NavLink to="Uberuns"> Über uns</NavLink>  </li>
-                             
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <ModalF />
-                <Routes>
-                    <Route path='/' element={<Startseite />} />
-                    <Route path='Uberuns' element={<Uberuns />} />
-                    <Route path='Stadte' element={<Stadte />} />
-                    <Route path='*' element={<NotFound />} />
-                    <Route path='Formular' element={<FormioForm />} />
+        <BrowserRouter>
 
-                
-                </Routes>
-            </BrowserRouter>
-        </>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container fluid>
+                    <Navbar.Brand href="#">  </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto"
+                            style={{ maxHeight: '100px'}}
+                            navbarScroll
+                        >
+                             <ModalF />
+                            <Nav.Link as={Link} to={"/Startseite"}> Startseite </Nav.Link>
+                            <Nav.Link  as={Link} to={"/Uberuns"}>Überuns</Nav.Link>
+                            <Nav.Link  as={Link} to={"/Stadte"}>Stadte</Nav.Link>
+                            <NavDropdown title="Link" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="Stadte">Stadte</NavDropdown.Item>
+                                <NavDropdown.Item href="Uberuns">Überuns</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="Formular"> Formular ausfüllen</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#" disabled>
+                            
+                            </Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="Suchen"
+                                placeholder="Suchen"
+                                className="me-2"
+                                aria-label="Suchen"
+                            />
+                            <Button variant="outline-success">Suchen</Button>
+                        </Form>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Routes>
+                <Route path='/' element={<Startseite />} />
+                <Route path='/Startseite' element={<Startseite />} />
+                <Route path='Uberuns' element={<Uberuns />} />
+                <Route path='Stadte' element={<Stadte />} />
+                <Route path='*' element={<NotFound />} />
+                <Route path='Formular' element={<FormioForm />} />
+            </Routes>
+        </BrowserRouter>
+
+
+
     );
 }
 
